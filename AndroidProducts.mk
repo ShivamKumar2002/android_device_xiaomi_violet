320c8c502c5fdef5482ce86b2b90758cd5caff2e
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2020-04-23 05:58:05 +0000

    Bootleggers Makeover

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_violet.mk
+    $(LOCAL_DIR)/bootleg_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_violet-userdebug \
-    revengeos_violet-eng \
-    revengeos_violet-user
+    bootleg_violet-userdebug \
+    bootleg_violet-eng \
+    bootleg_violet-user
